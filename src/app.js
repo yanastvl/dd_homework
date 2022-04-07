0a9973f7b99f0445ab02e2fe3c783aaee1956afe
Yana Stoyanidu <yana955@mail.ru>
2022-04-07 20:17:51 +0300

    add comments

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import './styles/style.scss';
 
-const projectName = "digital_design_homework"
+// Объявляем слайдер с изображениями и кнопками под ним
 const slider = document.querySelector('.main__img');
 const tabs = document.getElementsByClassName('tabs');
 
+// Объявляем номер текущего слайда и добавляем класс для стилизации активной кнопки слайда
 let currentSlideNumber = 0;
 tabs[currentSlideNumber].classList.add('tab__active');
 
+// Функция, принимающая номер слайда и меняющая изображение и цвет кнопки
 function changeSlide(newSlideNumber) {
     if (newSlideNumber > tabs.length - 1) {
         currentSlideNumber = 0;
@@ -24,11 +26,14 @@ function changeSlide(newSlideNumber) {
     tabs[currentSlideNumber].classList.add('tab__active');
 }
 
+// Назначаем обработчик события на все кнопки слайдера
 Array.from(tabs).forEach(function(tab, i) {
     tab.addEventListener('click', () => changeSlide(i));
 });
 
+// Инициализируем кнопки для мобильной версии и назначаем на них обработчик события для смены слайдов 'вперед-назад'
 document.getElementById('prev').addEventListener('click', () => changeSlide(currentSlideNumber -= 1));
 document.getElementById('next').addEventListener('click', () => changeSlide(currentSlideNumber += 1));
 
+// Подключаем таймер, который каждые 3.5 секунды сменяет слайд
 setInterval(() => changeSlide(currentSlideNumber += 1), 3500);
